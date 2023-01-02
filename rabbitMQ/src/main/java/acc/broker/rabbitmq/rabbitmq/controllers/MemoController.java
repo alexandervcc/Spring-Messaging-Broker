0bb825c65b626880928b69e85e10569558b278d2
Alexander Cuenca <alex.charco.25@gmail.com>
2023-01-02 12:28:36 -0500

    Memo Pull receiver

--- rabbitMQ/src/main/java/acc/broker/rabbitmq/rabbitmq/controllers/MemoController.java
+++ rabbitMQ/src/main/java/acc/broker/rabbitmq/rabbitmq/controllers/MemoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import acc.broker.rabbitmq.rabbitmq.components.MemoReceiver;
 import acc.broker.rabbitmq.rabbitmq.model.Memo;
 import acc.broker.rabbitmq.rabbitmq.services.interfaces.MemoService;
 import lombok.AllArgsConstructor;
@@ -16,16 +17,19 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class MemoController {
     private final MemoService memoService;
+    private final MemoReceiver receiver;
 
     @PostMapping(value = "/")
     public ResponseEntity<Memo> postMemo(@RequestBody Memo memo) {
         this.memoService.sendMemo(memo);
+        this.receiver.receiveMemo();
         return ResponseEntity.status(HttpStatus.OK).body(memo);
     }
 
     @PostMapping(value = "/convert")
     public ResponseEntity<Memo> postConvertMemo(@RequestBody Memo memo) {
         this.memoService.convertAndSendMemo(memo);
+       
         return ResponseEntity.status(HttpStatus.OK).body(memo);
     }
 }
